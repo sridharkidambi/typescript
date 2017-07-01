0f47648c9b40febe91c28d33f854c6b106b98aad
Sridhar Kidambi <sridharkidambi@srinivasas-mbp.attlocal.net>
2017-06-30 20:59:10 -0500

    typescript basics

--- helloworld.ts
+++ helloworld.ts
@@ -1,7 +1,23 @@
- class hello{
-    name:string;
-    constructor()
-    {
-        this.name="hello Sridhar";
-    }
+//   class hello{
+//     name:string;
+//     public constructor()
+//     {
+//         this.name="hello Sridhar!!!";
+//         console.log(this.name);
+//     }
+// }
+
+function hime(){
+    console.log("hello Sridhar!!!");
 }
+hime();
+
+var text1:string ="Sridhar";
+let text2:boolean=true;
+const text3:number =1.3;
+
+var msg:string =" i am giving space ";
+console.log(text1+msg+text2+msg+text3);
+
+var msg1:string =`i am giving ${msg} space `;
+console.log(msg1);
