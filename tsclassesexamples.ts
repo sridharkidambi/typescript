f703e6a5a429641a82892cc4656ddbfefab0d521
Sridhar Kidambi <sridharkidambi@srinivasas-mbp.attlocal.net>
2017-07-03 07:46:04 -0500

    classes and inheritance

--- tsclassesexamples.ts
+++ tsclassesexamples.ts
@@ -1,10 +1,86 @@
 class samplec {
 
      fullname:string="";
-    constructor(public name,public state,public city){
+    constructor(public name:string,public state:string,public city:string){
             this.fullname=name+"-"+state+"-"+city;
     }
 }
 
 var objsamplec=new samplec("A","B","C");
 console.log(objsamplec.fullname);
+
+class inhersamplec extends samplec{
+    mycount:number=0;
+    constructor(public name:string,public state:string,public city:string,public count:number)
+    { 
+        super(name,state,city);
+        this.mycount=count;
+        
+    }
+
+    printfinalnamecount(){
+        console.log(this.fullname+" prinintFinal "+this.mycount);
+    }
+}
+var objinhersamplec=new inhersamplec("a","b","c",5);
+objinhersamplec.printfinalnamecount();
+console.log(objinhersamplec.count);
+var realusers={
+    email:"k.s@yahoo.com",
+    firsntmae:"sridhar",
+    lastName:"kidambi",
+    sayhi(){
+        console.log("say hi");
+    }
+}
+console.log(realusers.email);
+realusers.sayhi();
+
+
+interface Users{
+    fname:string;
+    lname?:string;
+    email?:string;
+}
+
+function userdtls(user:Users):string{
+    return `the user name is ${user.fname}   ${user.lname}  ${user.email}`;
+}
+var newuser={
+    fname:"fname",
+    // lname:"lname",
+    email:"email"
+}
+console.log(userdtls(newuser));
+
+interface InvoiceT{
+    (name:string,total:number):void;
+}
+var myinvoice:InvoiceT;
+myinvoice=function(a,t){
+    console.log(a);
+     console.log(t);
+}
+
+namespace posting{
+export interface Ipost{
+    name:string;
+    email:string;
+}
+
+ export class post implements Ipost{
+    name:string;
+    email:string;
+    constructor(opost:Ipost){
+        this.name=opost.name;
+        this.email=opost.email;
+    }
+
+    printpos(){
+        console.log(this.name);
+        console.log(this.email);
+    }
+}
+}
+var opost= new posting.post({email:"s.k@gmail.com",name:"pname"});
+opost.printpos();
