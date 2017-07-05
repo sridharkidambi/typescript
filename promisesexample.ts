d9a60853c16d3321ccfd996f22cb5b4664a0ccb4
Sridhar Kidambi <sridharkidambi@srinivasas-mbp.attlocal.net>
2017-07-05 00:39:57 -0500

    promis call example

---
+++ promisesexample.ts
@@ -0,0 +1,38 @@
+"use strict"
+
+let performupload= function(imagestatus:string): Promise<{imagestatus:string,name:number}>{
+
+return new Promise((resolved)=>{
+    console.log(`Status: ${imagestatus}`)
+    resolved({ imagestatus:"a",name:5} );
+    // setTimeout(() =>{
+    //     imagestatus= imagestatus+"success";
+    //     resolved({imagestatus:imagestatus});
+    // }, 1000);
+    //  setTimeout(() =>{
+    //     resolve({imagestatus:imagestatus+"error"});
+    // }, 1000);
+});
+
+}
+
+var upload,compress,transfer;
+
+performupload('uploading')
+.then((res)=>{
+    upload=res;
+    console.log(res);
+    return performupload('compressing');
+}).then((res)=>{
+    compress=res;
+     console.log(res);
+    return performupload('transfer');
+}).then((res)=>{
+    upload=res;
+     console.log(res);
+    throw Error;
+    // return performupload('done');
+}).catch((res)=>{
+    console.log("errror");
+     performupload('done');
+})
